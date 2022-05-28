8328021021d9bc39dc5bb6ab69132f99477fd4e4
Juan Mari <juannpmari@gmail.com>
2022-05-27 23:39:23 -0300

    navigation drawer

--- app/src/main/java/com/argentinapesca/argentinapesca/ui/MainActivity.kt
+++ app/src/main/java/com/argentinapesca/argentinapesca/ui/MainActivity.kt
@@ -3,7 +3,10 @@ package com.argentinapesca.argentinapesca
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.argentinapesca.argentinapesca.data.model.Post
 import com.argentinapesca.argentinapesca.data.remote.DataSource
 import com.argentinapesca.argentinapesca.presentation.PostViewModel
@@ -18,10 +21,32 @@ import com.argentinapesca.argentinapesca.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.item1 -> Toast.makeText(applicationContext,"Clickeado item 1",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
