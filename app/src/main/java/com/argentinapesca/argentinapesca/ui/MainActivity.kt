5247fc7eb9ece4cb69913a0d5ef99f7fe9e3696f
Juan Mari <juannpmari@gmail.com>
2022-05-28 01:35:13 -0300

    drave

--- app/src/main/java/com/argentinapesca/argentinapesca/ui/MainActivity.kt
+++ app/src/main/java/com/argentinapesca/argentinapesca/ui/MainActivity.kt
@@ -2,20 +2,10 @@ package com.argentinapesca.argentinapesca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.argentinapesca.argentinapesca.data.model.Post
-import com.argentinapesca.argentinapesca.data.remote.DataSource
-import com.argentinapesca.argentinapesca.presentation.PostViewModel
-import com.argentinapesca.argentinapesca.presentation.PostViewModelFactory
-import androidx.fragment.app.viewModels
-import com.google.firebase.firestore.FirebaseFirestore
-//import kotlinx.coroutines.*
-//import java.util.*
-import androidx.lifecycle.Observer
+import androidx.drawerlayout.widget.DrawerLayout
 import com.argentinapesca.argentinapesca.databinding.ActivityMainBinding
 
 
@@ -25,18 +15,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
-        binding.drawerLayout.addDrawerListener(toggle)
+
+        setContentView(binding.root)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-
         binding.navView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.item1 -> Toast.makeText(applicationContext,"Clickeado item 1",Toast.LENGTH_SHORT).show()
+            when (it.itemId) {
+                R.id.item1 -> Toast.makeText(
+                    applicationContext,
+                    "Clickeado item 1",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
             true
         }
