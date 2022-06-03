4f4d8f92d52636bee9db26477e716023a26ab9d3
Juan Mari <juannpmari@gmail.com>
2022-06-03 15:01:22 -0300

    1 img uri

--- app/src/main/java/com/argentinapesca/argentinapesca/ui/newPost/newPostFragment.kt
+++ app/src/main/java/com/argentinapesca/argentinapesca/ui/newPost/newPostFragment.kt
@@ -1,13 +1,20 @@
 package com.argentinapesca.argentinapesca.ui.newPost
 
 import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -24,8 +31,14 @@ import com.argentinapesca.argentinapesca.presentation.newPost.newPostViewModelFa
 import com.argentinapesca.argentinapesca.repository.home.RepositoryImpl
 import com.argentinapesca.argentinapesca.repository.newPost.newPostRepository
 import com.argentinapesca.argentinapesca.repository.newPost.newPostRepositoryImpl
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.imaginativeworld.whynotimagecarousel.utils.setImage
 import java.net.URI
 import java.util.Arrays.toString
+import javax.security.auth.callback.Callback
+import kotlin.properties.Delegates
 
 class newPostFragment : Fragment(R.layout.fragment_new_post) {
 
@@ -36,22 +49,30 @@ class newPostFragment : Fragment(R.layout.fragment_new_post) {
             )
         )
     }
-    private lateinit var binding: FragmentNewPostBinding
+
+    companion object {
+        private lateinit var binding: FragmentNewPostBinding
+        var imageUri by Delegates.observable(Uri.EMPTY) { prop, old, new ->
+            Log.d("uri", "Nuevo valor $new")
+            binding.imgGallery.setImageURI(new)
+        }
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentNewPostBinding.bind(view)
+        binding = FragmentNewPostBinding.bind(view)
         val img = mutableListOf<String>()
 
+
         binding.btnGallery.setOnClickListener {
-            val intent = Intent()
-            //intent.setType("image/*")
-            intent.setAction(Intent.ACTION_GET_CONTENT)
-            var uri:Uri=Uri.EMPTY
-            intent.setDataAndType(uri,"image/*")
-            resultLauncher.launch(intent)
-            binding.imgGallery.setImageURI(intent.data)
+            resultLauncher.launch("image/*")
+            if (imageUri != Uri.EMPTY) {
+                //Log.d("uri", "2 uri: ${imageUri.toString()}")
+                binding.imgGallery.setImageURI(imageUri)
+            }
         }
+
 
         binding.btnCreate.setOnClickListener {
             img.add(binding.editImage1.text.toString())
@@ -65,16 +86,30 @@ class newPostFragment : Fragment(R.layout.fragment_new_post) {
             findNavController().popBackStack()
         }
 
+
     }
 
-    /**/
-    var resultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                // There are no request codes
-                //val data: Intent? = result.data
-                //binding.imgGallery.setImageURI(data?.getData())
+
+    var resultLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.GetContent(), Callback)
+
+    /*{ result ->
+    if (result.resultCode == Activity.RESULT_OK) {
+        // There are no request codes
+        val data: Intent? = result.data
+
+        //Log.d("URI","${data?.data.toString()}")
+       // bitmap=data?.data as Bitmap
+        //binding.imgGallery.setImageURI(data?.getData())
+    }
+}*/
+    object Callback : ActivityResultCallback<Uri> {
+        override fun onActivityResult(result: Uri?) {
+            if (result != null) {
+                imageUri = result
+                //Log.d("uri", "${imageUri.toString()}")
             }
         }
+    }
 }
 
